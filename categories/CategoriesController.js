86d4720f87afefe85df0078301e2ec5529f6a29a
Gabriel Novaes <semproblema@gmail.com>
2022-02-11 18:18:01 -0400

    Save method for category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -9,6 +9,13 @@ router.get("/admin/categories/new",(req,res) => {
     res.render("admin/categories/new")
 });
 
+
+router.get("/admin/categories",(req,res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/list", {categories:categories} );
+    })
+});
+
 router.post("/admin/categories/save",(req,res) =>{
     var title = req.body.title;
 
@@ -17,7 +24,7 @@ router.post("/admin/categories/save",(req,res) =>{
             title:title,
             slug: slugify(title)
         }).then( () =>{
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     }else{
         res.redirect("/admin/categories/new")
