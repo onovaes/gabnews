bbddee3313a5f25e067de2f9b114943d2a481be5
Gabriel Novaes <semproblema@gmail.com>
2022-02-11 17:42:52 -0400

    Correct view folder for admin and create new category

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/articles",(req,res) => {
-    res.send("Router Articles here")
+    res.send("Router Articles")
 });
 
 module.exports = router;
