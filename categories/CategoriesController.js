7cf291ca802c8dcd132fd73d9870857c4944fde2
Gabriel Novaes <semproblema@gmail.com>
2022-02-11 17:58:52 -0400

    Install slugfy and create save method for categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,27 @@
+const e = require("express");
 const express = require("express");
 const router = express.Router();
+const slugify = require("slugify");
+
+const Category = require("./Category");
 
 router.get("/admin/categories/new",(req,res) => {
     res.render("admin/categories/new")
 });
+
+router.post("/admin/categories/save",(req,res) =>{
+    var title = req.body.title;
+
+    if (title != undefined){
+        Category.create({
+            title:title,
+            slug: slugify(title)
+        }).then( () =>{
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+})
 
 module.exports = router;
